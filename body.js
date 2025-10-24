100210ff8dba06b4db9d46560d960de8a743f7b5
varunsarmab2205 <varunsarmab22@gmail.com>
2025-10-24 11:55:34 +0530

    Create body.js

---
+++ body.js
@@ -0,0 +1,49 @@
+const Body=()=>{
+    return(
+       <main>
+        <div className="container2">
+        <div className="movie1">
+       <img src="https://play-lh.googleusercontent.com/proxy/IZrC_MbcA5TI9Z6mSng2Yqak_P_BKX9gLiIQ1JM7tvvU_4lkAnUNp6MnuQpmABTdWoDhNYj9fiD8ArVBV12vmCMIHBYEkbyVDn0LVLoaYhqRYqhFOiIr5SKrH1spVSru7dDNtA5ORm0DbTb73jk9WBXVzDOKzjRWLP_PHg" />
+          <p className="movie-title">Pellichoopulu</p>
+          <p className="Cast">Vijay Devarakonda, Rithu Varma, Director - Tharun Bhascker Dhaassyam</p>
+           <p>Rating: 9.5</p>
+        </div>
+
+         <div className="movie2">
+           < img src="https://upload.wikimedia.org/wikipedia/en/thumb/c/c5/Premalu_film_poster.jpg/250px-Premalu_film_poster.jpg" />
+          <p className="movie-title">Premalu</p>
+           <p className="Cast">Nasleen, Mamitha Baiju,  Director - Girish A.D</p>
+           <p>Rating: 8.5</p>
+           </div>
+
+         <div className="movie3">
+             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSzkllpnboLso4ekC4T6eCI7FDn2AC05_xxlZnPtFGf6cEpAQHLJVkETbc_TSKXylSVR4&usqp=CAU" alt="Kaithi" />
+          <p className="movie-title">Kaithi</p>
+          <p className="Cast">Karthi,  Director - Lokesh Kanagaraj</p>
+          <p>Rating: 9.0</p>
+         
+           </div>
+
+         <div className="movie4">
+            <img src="https://jmmnewaov2.wordpress.com/wp-content/uploads/2013/12/wus-cver.jpeg?w=640" alt="Wakeup Sid" />
+           <p classname="movie-title">Wakeup Sid</p>
+           <p classname="Cast">Ranbir Kapoor, Konkona Sen Sharma,   Director - Ayaan Mukherjee</p>
+           <p>Rating: 8.3</p>
+               </div>
+
+         <div className="movie5"> 
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTEukgZef7FjKoOjgLW3l5P-RO0TEg4bVoNAA&s" alt="Kantara" />
+          <p className="movie-title">Kantara</p>
+          <p className="Cast">Rishabh Shetty, Sapthami Gowda,   Director - Rishabh Shetty</p>
+          <p>Rating: 8.5</p>
+           </div>
+
+           </div>
+
+</main>
+
+       );
+};
+
+
+export default Body;
